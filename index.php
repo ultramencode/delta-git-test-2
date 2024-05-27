afb40981feaf1123a64dac282fb0a391bda87380
M Aris Fauzi <arisfauzi.me@gmail.com>
2024-05-27 13:16:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, autem.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 </body>
 </html>
